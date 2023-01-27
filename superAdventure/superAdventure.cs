ccdb57b0807797fd91f95412793ffaa2673092ce
aditore <anthonyditore@gmail.com>
2023-01-26 18:50:25 -0800

    Add project files.

---
+++ superAdventure/superAdventure.cs
@@ -0,0 +1,36 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace superAdventure
+{
+    public partial class superAdventure : Form
+    {
+        private Player _player;
+        public superAdventure()
+        {
+            InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+    }
+}
